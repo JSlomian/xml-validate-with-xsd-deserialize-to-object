a148b369d4c52344d72ec184566be1ebaf995c29
Jan Slomian <jslomian@multiprojekt.pl>
2022-10-16 11:59:43 +0200

    commit

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [XmlTypeAttribute(AnonymousType = true)]
+    [XmlRootAttribute(Namespace = "http://moje.dane.com", ElementName = "book", IsNullable = false)]
+    public class Book
+    {
+        [XmlElement(IsNullable = false)]
+        private string authorField;
+        [XmlElement(IsNullable = false)]
+        private string titleField;
+        [XmlElement(IsNullable = false)]
+        private ushort yearField;
+
+        public string Author
+        {
+            get
+            {
+                return this.authorField;
+            }
+            set
+            {
+                this.authorField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Title
+        {
+            get
+            {
+                return this.titleField;
+            }
+            set
+            {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public ushort Year
+        {
+            get
+            {
+                return this.yearField;
+            }
+            set
+            {
+                this.yearField = value;
+            }
+        }
+    }
+
+
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        private string xmlFileName = "dane.xml";
+        private string xsdFileName = "dane.xsd";
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            if (p.Validate())
+            {
+                p.Deserialize();
+            }
+        }
+
+        private bool Validate()
+        {
+            XDocument xDoc = XDocument.Load(xmlFileName);
+            XmlSchemaSet Schema = new XmlSchemaSet();
+            Schema.Add("http://moje.dane.com", xsdFileName);
+            try
+            {
+                xDoc.Validate(Schema, null);
+            }
+            catch (XmlSchemaValidationException e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("invalid");
+                return false;
+            }
+            return true;
+        }
+
+        private void Deserialize()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Book));
+            Book book;
+
+            using (Stream xmlReader = new FileStream(xmlFileName, FileMode.Open, FileAccess.Read))
+            {
+                book = (Book)xmlSerializer.Deserialize(xmlReader);
+
+                Console.Write(book.Title);
+                Console.Write(" - ");
+                Console.Write(book.Author);
+                Console.Write(" - ");
+                Console.Write(book.Year);
+                Console.WriteLine();
+            }
+        }
+    }
+}
